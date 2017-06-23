a9cf1ca22a88500433969795e913b6bccdea2f0e
Jamie Doyle <thedoyles922@users.noreply.github.com>
2017-06-23 16:35:02 -0400

    installed busboy, able to track and display letters guessed

--- app.js
+++ app.js
@@ -2,14 +2,50 @@ var express = require('express');
 var mustacheExpress = require('mustache-express');
 var bodyParser = require('body-parser');
 var path = require('path');
-var sessions = require('express-session');
+var session = require('express-session');
 var parseurl = require('parseurl');
+var fs = require('fs');
+var busboy = require('busboy');
 
 var app = express();
 
+app.engine('mustache', mustacheExpress());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'mustache');
 
-app.get('/index/', function (req, res) {
-  res.send('hello');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: 'Covfefe',
+  resave: false,
+  saveUninitialized: true
+}));
+
+var words = {
+  wordsArr: [
+      {'words': fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n")}
+  ]
+};
+var numGuesses = [8];
+var guessedLetters = [];
+
+
+
+app.get('/', function (req, res) {
+  context = {
+    numGuesses: numGuesses,
+    guessedLetters: guessedLetters
+  };
+  res.render('index', context);
+});
+
+app.post('/', function (req, res) {
+  var letter = req.body.letter;
+  console.log(letter);
+  guessedLetters.push(letter);
+  res.redirect('/');
 });
 
 app.listen(3000, function () {
